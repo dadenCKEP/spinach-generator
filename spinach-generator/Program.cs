d4b16491028235f4d0f187350db2903fa0bdc6b5
dadenCKEP <dadenCKEP@users.noreply.github.com>
2020-09-07 18:55:03 +0900

    個人設定クラスを作成

--- spinach-generator/Program.cs
+++ spinach-generator/Program.cs
@@ -6,17 +6,38 @@ using System.Windows.Forms;
 
 namespace spinach_generator
 {
+    public class NippouSettings
+    {
+        public string nippouBasePath { get; set; }
+        public string nippouDirName { get; set; }
+        public string shuhouDirName { get; set; }
+        public string userName { get; set; }
+    }
+
     static class Program
     {
         /// <summary>
         /// アプリケーションのメイン エントリ ポイントです。
         /// </summary>
+        /// 
+
+        // 個人設定
+        public static NippouSettings nippouSettings = new NippouSettings();
+
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+        // 設定を読み込む
+
+        // なさそうなら専用フォームで聞く
+
+        // 
+
+
+        Application.Run(new Form1());
         }
-    }
+}
 }
